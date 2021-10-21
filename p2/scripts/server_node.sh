401340e3cf5960c45cd20b40cf0eca7951132986
MOUTIK ABDELKARIM <abdelkarimoutik@gmail.com>
2021-10-21 15:18:01 +0100

    feat: part two of IoT

---
+++ p2/scripts/server_node.sh
@@ -0,0 +1,18 @@
+echo "installing k3s..."
+
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $1 --node-ip $1 --flannel-iface=eth1"
+
+curl -sfL https://get.k3s.io | sh -
+
+echo "create k3s alias"
+echo 'alias k="k3s kubectl"' >> /home/vagrant/.bashrc
+echo "server installed"
+
+echo "Creating 3 deployments...."
+/usr/local/bin/kubectl apply -f /vagrant/config/k3s/deployments/
+echo "Creating 3 services...."
+/usr/local/bin/kubectl apply -f /vagrant/config/k3s/services/
+echo "Creating ingress Route...."
+/usr/local/bin/kubectl apply -f /vagrant/config/k3s/Ingress/apps-ingress.yaml
+
+echo "Happy Hacking!"
