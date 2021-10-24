16781cb73ca7d940a306650455dc79e0e0b77a35
MOUTIK ABDELKARIM <abdelkarimoutik@gmail.com>
2021-10-24 15:33:34 +0100

    feat: add part 3 config file

--- p3/config/neededcommands.sh
+++ p3/config/neededcommands.sh
@@ -18,13 +18,22 @@ sudo usermod -aG docker ${USER}
 
 #install k3d
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
-k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8443:443@loadbalancer
+# k3d cluster create dev-cluster --port 8888:8888@loadbalancer 8080:80@loadbalancer --port 8443:443@loadbalancer
+k3d cluster create --config k3d.yml
+
 
 #set up argo
 kubectl create namespace argocd
 kubectl create namespace dev
 kubectl apply -n argocd -f /vagrant/config/install.yaml
+
+# we must for the jobs to finish
+kubectl -n argocd wait --for=condition=complete --timeout=60s jobs/helm-install-traefik-crd
+
 kubectl apply -n argocd -f /vagrant/config/ingress.yaml
+
+# wait for the argocd customer resources to be installed
+sleep 60
 
 #this will help
 #https://www.techmanyu.com/setup-a-gitops-deployment-model-on-your-local-development-environment-with-k3s-k3d-and-argocd-4be0f4f30820
